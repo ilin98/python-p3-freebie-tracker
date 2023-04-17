438815d3fee5479c99ac7f3a3c3b56cdcd4e44c4
Isaac <isaac.lin.pnc@gmail.com>
2023-04-17 11:24:22 -0400

    finnished lab

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -16,6 +17,10 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebies', backref=backref('company'))
+    devs = association_proxy('freebies', 'dev',
+        creator=lambda us: Freebies(dev=us))
+
     def __repr__(self):
         return f'<Company {self.name}>'
 
@@ -25,5 +30,22 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebies', backref=backref('dev'))
+    companies = association_proxy('freebies', 'company',
+        creator=lambda us: Freebies(company=us))
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+class Freebies(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}>'
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+from random import choice as rc
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Dev, Company, Freebies
+
+engine = create_engine('sqlite:///seed_db.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def delete_records():
+    session.query(Dev).delete()
+    session.query(Company).delete()
+    session.query(Freebies).delete()
+    session.commit()
+
+def create_records():
+    companies = [Company() for i in range(500)]
+    devs = [Dev() for i in range(500)]
+    freebies = [Freebies() for i in range(1000)]
+    session.add_all(companies + devs + freebies)
+    session.commit()
+    return companies, devs, freebies
+
+def relate_one_to_many(devs, companies, freebies):
+    for freebie in freebies:
+        freebie.dev = rc(devs)
+        freebie.company = rc(companies)
+
+    session.add_all(freebies)
+    session.commit()
+
+if __name__ == '__main__':
+    delete_records()
+    devs, companies, freebies = create_records()
+    relate_one_to_many(devs, companies, freebies)
